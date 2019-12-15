a5c4f3dd029449433c13428f0f5933957fc6c23d
Felipe Marinho <felipmarinho@gmail.com>
2019-12-15 15:23:31 -0300

    Adicionando o eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -34,6 +35,11 @@ module.exports = {
               extensions: [ '.jsx', '.js']
           }
       ],
-      'import/prefer-default-export': 'off'
+      'import/prefer-default-export': 'off',
+      'no-param-reassign': 'off',
+      'no-console': ['error', { allow: ['tron']}],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
   },
 };
