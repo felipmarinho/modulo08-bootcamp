7e1c47c84c54b7b9bbc71a8d7f5e50f3e034ae4a
Felipe Marinho <felipmarinho@gmail.com>
2019-12-15 16:12:30 -0300

    Usando o Hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  /**
+   * é executado uma unica vez já que não fica observando/escutando nenhum atributo
+   */
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  /**
+   * é executado sempre que o tech for alterado,
+   * pois ele fica observando/escutando o atributo "tech"
+   */
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
